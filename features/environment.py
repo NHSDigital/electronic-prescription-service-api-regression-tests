cb5c8dedc86966a8686a5d325aafe4ccc1370ba4
natasafrgk <natasa.fragkou@airelogic.com>
2024-01-23 15:46:43 +0000

    reverts environment py

--- features/environment.py
+++ features/environment.py
@@ -18,9 +18,9 @@ def before_all(context):
     else:
         setup_logging(level=INFO)
         
-    #eps_pr_suffix = build_pull_request_id(PULL_REQUEST_ID)
-
-    context.base_url = BASE_URL + "electronic-prescriptions"
+    eps_pr_suffix = build_pull_request_id(PULL_REQUEST_ID)
+    
+    context.base_url = BASE_URL + "electronic-prescriptions" + eps_pr_suffix
     
     logging.info("Using BASE_URL: '%s'", context.base_url)
 
