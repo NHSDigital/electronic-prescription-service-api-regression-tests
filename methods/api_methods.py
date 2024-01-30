5905bb95409a5d08ff3873c1c40d8bd660d0d2a0
SeanSteberis <sean.steberis@airelogic.com>
2024-01-30 18:50:57 +0000

    changed the way api information is attached and assertion information

--- methods/api_methods.py
+++ methods/api_methods.py
@@ -5,8 +5,6 @@ from assertpy import assert_that as assertpy_assert  # type: ignore
 
 
 def assert_that(expected, context):
-    allure.attach(str(expected), "Expected", allure.attachment_type.TEXT)
-    allure.attach(str(context.response), "Actual", allure.attachment_type.TEXT)
     description = (
         f"Actual Response: Status Code: {context.response.status_code}, "
         f"Body: {context.response.content}"
@@ -15,13 +13,33 @@ def assert_that(expected, context):
 
 
 def attach_api_information(context):
-    allure.attach(str(context.response.request.headers), "REQUEST Headers", allure.attachment_type.TEXT)
-    allure.attach(str(context.response.request.method), "REQUEST Method", allure.attachment_type.TEXT)
-    allure.attach(str(context.response.request.url), "REQUEST URL", allure.attachment_type.TEXT)
-    allure.attach(str(context.response.request.body), "REQUEST Body", allure.attachment_type.TEXT)
-    allure.attach(str(context.response.status_code), "RESPONSE Status Code", allure.attachment_type.TEXT)
-    allure.attach(str(context.response.headers), "RESPONSE Headers", allure.attachment_type.TEXT)
-    allure.attach(str(context.response.content), "RESPONSE Body", allure.attachment_type.TEXT)
+    allure.attach(
+        str(context.response.request.headers),
+        "REQUEST Headers",
+        allure.attachment_type.TEXT,
+    )
+    allure.attach(
+        str(context.response.request.method),
+        "REQUEST Method",
+        allure.attachment_type.TEXT,
+    )
+    allure.attach(
+        str(context.response.request.url), "REQUEST URL", allure.attachment_type.TEXT
+    )
+    allure.attach(
+        str(context.response.request.body), "REQUEST Body", allure.attachment_type.JSON
+    )
+    allure.attach(
+        str(context.response.status_code),
+        "RESPONSE Status Code",
+        allure.attachment_type.TEXT,
+    )
+    allure.attach(
+        str(context.response.headers), "RESPONSE Headers", allure.attachment_type.TEXT
+    )
+    allure.attach(
+        str(context.response.content), "RESPONSE Body", allure.attachment_type.JSON
+    )
 
 
 def get_default_headers():
@@ -35,10 +53,11 @@ def request_ping(context):
     url = f"{context.fhir_base_url}/_ping"
     print(f"going to {url}")
     context.response = requests.get(url=url)
-    # attach_api_information(context)
+    attach_api_information(context)
 
 
 def the_expected_response_code_is_returned(context, expected_response_code: int):
-    assert_that(context.response.status_code, context).is_equal_to(
-        expected_response_code
-    )
+    actual_response_code = context.response.status_code
+    allure.attach(str(expected_response_code), "Expected", allure.attachment_type.TEXT)
+    allure.attach(str(actual_response_code), "Actual", allure.attachment_type.TEXT)
+    assert_that(actual_response_code, context).is_equal_to(expected_response_code)
