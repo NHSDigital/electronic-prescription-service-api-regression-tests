0571d56ce704ce7db2560ec7cfe4337e37f11705
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-02-14 15:42:35 +0000

    use seperate environment for pull requests (#66)
    ## Summary
    
    - Routine Change
    
    
    ### Details
    
    - use separately named environment for pull requests
    
    ---------
    
    Co-authored-by: SeanSteberis <sean.steberis@airelogic.com>

--- features/environment.py
+++ features/environment.py
@@ -3,12 +3,12 @@ import os
 import sys
 
 
-INTERNAL_QA_BASE_URL = "Https://internal-qa.api.service.nhs.uk/"
-INTERNAL_DEV_BASE_URL = "Https://internal-dev.api.service.nhs.uk/"
-INT_BASE_URL = "Https://int.api.service.nhs.uk/"
-SANDBOX_DEV_BASE_URL = "Https://internal-dev-sandbox.api.service.nhs.uk/"
-SANDBOX_INT_BASE_URL = "Https://sandbox.api.service.nhs.uk/"
-REF_BASE_URL = "Https://ref.api.service.nhs.uk/"
+INTERNAL_QA_BASE_URL = "https://internal-qa.api.service.nhs.uk/"
+INTERNAL_DEV_BASE_URL = "https://internal-dev.api.service.nhs.uk/"
+INT_BASE_URL = "https://int.api.service.nhs.uk/"
+SANDBOX_DEV_BASE_URL = "https://internal-dev-sandbox.api.service.nhs.uk/"
+SANDBOX_INT_BASE_URL = "https://sandbox.api.service.nhs.uk/"
+REF_BASE_URL = "https://ref.api.service.nhs.uk/"
 
 ENVS = {
     "INTERNAL-DEV": INTERNAL_DEV_BASE_URL,
@@ -19,6 +19,10 @@ ENVS = {
     "SANDBOX": SANDBOX_INT_BASE_URL,
 }
 
+REPOS = {
+    "EPS-FHIR": "https://github.com/NHSDigital/electronic-prescription-service-api"
+}
+
 # This will need rework when the pack includes additional products to test
 PULL_REQUEST_ID = os.getenv("PULL_REQUEST_ID")
 EPS_SUFFIX = "electronic-prescriptions"
@@ -27,11 +31,11 @@ EPS_SUFFIX = "electronic-prescriptions"
 def before_all(context):
     env = context.config.userdata["env"].upper()
 
-    context.fhir_base_url = select_base_url(env) + EPS_SUFFIX
+    context.fhir_base_url = os.path.join(select_base_url(env), EPS_SUFFIX)
     # This will need rework when the pack includes additional products to test
     if PULL_REQUEST_ID:
-        context.fhir_base_url = (
-            INTERNAL_DEV_BASE_URL + EPS_SUFFIX + build_pull_request_id()
+        context.fhir_base_url = os.path.join(
+            INTERNAL_DEV_BASE_URL, f"{EPS_SUFFIX}-{PULL_REQUEST_ID}"
         )
 
     logging.info("Using BASE_URL: '%s'", context.fhir_base_url)
@@ -41,7 +45,19 @@ def after_all(context):
     # Add anything you want to happen after all the tests have completed here
     env = context.config.userdata["env"].upper()
     product = context.config.userdata["product"].upper()
-    properties_dict = {"PRODUCT": product, "ENV": env, "PR_ID": PULL_REQUEST_ID}
+    properties_dict = {"PRODUCT": product, "ENV": env}
+    if PULL_REQUEST_ID:
+        env = os.path.join("PULL-REQUEST", PULL_REQUEST_ID)
+        pull_request_link = os.path.join(
+            select_repository_base_url(product),
+            "pull",
+            PULL_REQUEST_ID.upper().replace("PR-", ""),
+        )
+        properties_dict = {
+            "PRODUCT": product,
+            "ENV": env,
+            "PULL-REQUEST": pull_request_link,
+        }
 
     file_path = "./allure-results/environment.properties"
     write_properties_file(file_path, properties_dict)
@@ -58,21 +74,18 @@ def setup_logging(level: int = logging.INFO):
     )
 
 
-def build_pull_request_id():
-    pr_suffix = ""
-    if PULL_REQUEST_ID:
-        if "pr-" in str({PULL_REQUEST_ID}):
-            pr_suffix = f"-{PULL_REQUEST_ID}"
-        else:
-            pr_suffix = f"-pr-{PULL_REQUEST_ID}"
-    return pr_suffix
-
-
 def select_base_url(env):
     if env in ENVS:
         return ENVS[env]
     else:
         raise ValueError(f"Unknown environment or missing base URL for: {env} .")
+
+
+def select_repository_base_url(product):
+    if product in REPOS:
+        return REPOS[product]
+    else:
+        raise ValueError(f"Unknown product or missing repository URL for: {product} .")
 
 
 def write_properties_file(file_path, properties_dict):
