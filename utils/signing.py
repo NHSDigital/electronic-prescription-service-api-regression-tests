923aee97a52fcc470960238e2e57abb981e02b8b
SeanSteberis <sean.steberis@airelogic.com>
2024-03-14 17:20:00 +0000

    fixed conflicts

--- utils/signing.py
+++ utils/signing.py
@@ -11,7 +11,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.serialization import Encoding, load_pem_private_key
 from cryptography.x509 import load_pem_x509_certificate
-from datetime import datetime
+import datetime
 import os
 from os.path import exists
 
@@ -31,12 +31,11 @@ X509_CERT_EXISTS = exists(X509_CERT_PATH)
 def get_signature(value_string: str, valid: bool):
     if not PRIVATE_KEY_EXISTS and X509_CERT_EXISTS:
         return DUMMY_SIGNATURE
-
     # load x509 and check it hasn't expired
     cert_bytes = load_file(X509_CERT_PATH)
     x509_cert = load_pem_x509_certificate(cert_bytes)
 
-    if x509_cert.not_valid_after < datetime.today():
+    if x509_cert.not_valid_after_utc < datetime.datetime.now(datetime.UTC):
         raise Exception("Signing certificate has expired")
 
     # load private key and generate signature
@@ -45,8 +44,10 @@ def get_signature(value_string: str, valid: bool):
 
     digest = base64.b64decode(value_string).decode("utf-8")
 
-    signature_raw = private_key.sign(
-        digest.encode("utf-8"), padding.PKCS1v15(), hashes.SHA1()
+    signature_raw = private_key.sign(  # pyright: ignore [reportAttributeAccessIssue, reportCallIssue]
+        digest.encode("utf-8"),
+        padding.PKCS1v15(),  # pyright: ignore [reportAttributeAccessIssue, reportCallIssue]
+        hashes.SHA1(),  # pyright: ignore [reportAttributeAccessIssue, reportCallIssue]
     )
 
     # align format of signature with equivalent ts code
@@ -74,7 +75,7 @@ def get_signature(value_string: str, valid: bool):
 
     # match returned signature data with that from equivalent ts code
     signature_data = base64.b64encode(xml_d_sig.encode("utf-8")).decode("utf-8")
-
+    print(signature_data)
     return signature_data
 
 
@@ -82,3 +83,10 @@ def load_file(path, mode="rb"):
     with open(path, mode) as f:
         doc = f.read()
     return doc
+
+
+if __name__ == "__main__":
+    get_signature(
+        "PFNpZ25lZEluZm8geG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvMDkveG1sZHNpZyMiPjxDYW5vbmljYWxpemF0aW9uTWV0aG9kIEFsZ29yaXRobT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS8xMC94bWwtZXhjLWMxNG4jIj48L0Nhbm9uaWNhbGl6YXRpb25NZXRob2Q+PFNpZ25hdHVyZU1ldGhvZCBBbGdvcml0aG09Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvMDkveG1sZHNpZyNyc2Etc2hhMSI+PC9TaWduYXR1cmVNZXRob2Q+PFJlZmVyZW5jZT48VHJhbnNmb3Jtcz48VHJhbnNmb3JtIEFsZ29yaXRobT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS8xMC94bWwtZXhjLWMxNG4jIj48L1RyYW5zZm9ybT48L1RyYW5zZm9ybXM+PERpZ2VzdE1ldGhvZCBBbGdvcml0aG09Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvMDkveG1sZHNpZyNzaGExIj48L0RpZ2VzdE1ldGhvZD48RGlnZXN0VmFsdWU+ZWFwYU9jaExDb05meGdjeHRHaGRvMXc0WHhFPTwvRGlnZXN0VmFsdWU+PC9SZWZlcmVuY2U+PC9TaWduZWRJbmZvPg==",
+        True,
+    )
