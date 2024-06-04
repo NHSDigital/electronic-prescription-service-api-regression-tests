25141a9970c5d5baebbeed63bdb48e1ea188a6df
Phil Gee <originalphil@gmail.com>
2024-06-04 09:02:47 +0000

    AEA-3869 Fix for getting env vars via dotenv.

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,8 @@
 import logging
 import os
 import sys
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-load_dotenv()
 
 INTERNAL_QA_BASE_URL = "https://internal-qa.api.service.nhs.uk/"
 INTERNAL_DEV_BASE_URL = "https://internal-dev.api.service.nhs.uk/"
@@ -31,11 +30,13 @@ REPOS = {
     "PFP-APIGEE": "https://github.com/NHSDigital/prescriptions-for-patients",
 }
 
-CERTIFICATE = os.getenv("CERTIFICATE")
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-PULL_REQUEST_ID = os.getenv("PULL_REQUEST_ID")
+env = dotenv_values(".env")
+CERTIFICATE = env.get("CERTIFICATE")
+PRIVATE_KEY = env.get("PRIVATE_KEY")
+CLIENT_ID = env.get("CLIENT_ID")
+CLIENT_SECRET = env.get("CLIENT_SECRET")
+PULL_REQUEST_ID = env.get("PULL_REQUEST_ID")
+
 EPS_FHIR_SUFFIX = "electronic-prescriptions"
 PFP_APIGEE_SUFFIX = "prescriptions-for-patients"
 
