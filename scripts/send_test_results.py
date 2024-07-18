f54c40c676074d88893a6470edd8e38771235233
Sean Steberis <103416906+seansteberisal@users.noreply.github.com>
2024-07-18 16:46:20 +0100

    Breaking: [AEA-3819] - Generate reports in a cleaner way (#140)
    ## Summary
    
    - :exclamation: Breaking Change
    - :robot: Operational or Infrastructure Change
    - :warning: Potential issues that might be caused by this change
    
    ### Details
    Instead of opening a new branch an uploading the test results to be
    processed. The action will now only upload the artifact and then send an
    API request to the test reports to begin the process of processing and
    uploading the results
    
    ## Pull Request Naming
    
    Pull requests should be named using the following format:
    
    ```text
    Tag: [AEA-NNNN] - Short description
    ```
    
    Tag can be one of:
    
    - `Fix` - for a bug fix. (Patch release)
    - `Update` - either for a backwards-compatible enhancement or for a rule
    change that adds reported problems. (Patch release)
    - `New` - implemented a new feature. (Minor release)
    - `Breaking` - for a backwards-incompatible enhancement or feature.
    (Major release)
    - `Docs` - changes to documentation only. (Patch release)
    - `Build` - changes to build process only. (No release)
    - `Upgrade` - for a dependency upgrade. (Patch release)
    - `Chore` - for refactoring, adding tests, etc. (anything that isn't
    user-facing). (Patch release)
    
    If the current release is x.y.z then
    - a patch release increases z by 1
    - a minor release increases y by 1
    - a major release increases x by 1
    
    Correct tagging is necessary for our automated versioning and release
    process ([Release](./RELEASE.md)).
    
    The description of your pull request will be used as the commit message
    for the merge, and also be included in the changelog. Please ensure that
    your title is sufficiently descriptive.
    
    ### Rerunning Checks
    
    If you need to rename your pull request, you can restart the checks by
    either:
    
    - Closing and reopening the pull request
    - pushing an empty commit 
      ```bash
      git commit --allow-empty -m 'trigger build'
      git push
      ```
    - Amend your last commit and force push to the branch
      ```bash
      git commit --amend --no-edit
      git push --force
      ```
    
    Rerunning the checks from within the pull request will not use the
    updated title.

---
+++ scripts/send_test_results.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import argparse
+import requests
+from requests.auth import HTTPBasicAuth
+
+URL = "https://api.github.com/repos/NHSDigital/eps-test-reports/actions/workflows/publish_report.yml/dispatches"
+
+
+def get_headers():
+    return {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+
+
+def get_auth_header():
+    user_credentials = arguments.user.split(":")
+    return HTTPBasicAuth(user_credentials[0], user_credentials[1])
+
+
+def trigger_run():
+    body = {
+        "ref": "main",
+        "inputs": {"run_id": run_id},
+    }
+
+    response = requests.post(
+        url=URL,
+        headers=get_headers(),
+        auth=get_auth_header(),
+        json=body,
+    )
+
+    assert (
+        response.status_code == 204
+    ), f"Failed to trigger test run. Expected 204, got {response.status_code}\nURL: {URL} \nBody: {response.text}"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--user", required=True, help="Please provide the user credentials."
+    )
+    parser.add_argument(
+        "--run_id", required=True, help="The ID of the workflow Run is Required"
+    )
+    arguments = parser.parse_args()
+    run_id = arguments.run_id
+
+    trigger_run()
+    print("Success!")
