0551a6340c0e732fc788c377dd6293505aa8c452
SeanSteberis <sean.steberis@airelogic.com>
2024-09-03 11:00:16 +0100

    changed pr url for PfP to AWS instead of APIGEE to work with ci

--- features/environment.py
+++ features/environment.py
@@ -11,6 +11,7 @@ load_dotenv(override=True)
 
 INTERNAL_QA_BASE_URL = "https://internal-qa.api.service.nhs.uk/"
 INTERNAL_DEV_BASE_URL = "https://internal-dev.api.service.nhs.uk/"
+PFP_AWS_PR_URL = "https://pfp-{{aws_pull_request_id}}.dev.eps.national.nhs.uk/"
 INT_BASE_URL = "https://int.api.service.nhs.uk/"
 SANDBOX_DEV_BASE_URL = "https://internal-dev-sandbox.api.service.nhs.uk/"
 SANDBOX_INT_BASE_URL = "https://sandbox.api.service.nhs.uk/"
@@ -75,8 +76,8 @@ def before_all(context):
                     INTERNAL_DEV_BASE_URL, f"{EPS_FHIR_SUFFIX}-{PULL_REQUEST_ID}"
                 )
             if product == "PFP-APIGEE":
-                context.pfp_apigee_base_url = os.path.join(
-                    INTERNAL_DEV_BASE_URL, f"{PFP_APIGEE_SUFFIX}-{PULL_REQUEST_ID}"
+                context.pfp_apigee_base_url = PFP_AWS_PR_URL.replace(
+                    "{{aws_pull_request_id}}", PULL_REQUEST_ID
                 )
     else:
         raise RuntimeError("no tests to run. Check your tags and try again")
