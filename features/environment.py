da0bf87c576e8703177854b26d6adccfde718128
SeanSteberis <sean.steberis@airelogic.com>
2024-09-16 10:51:57 +0100

    fix linting issue

--- features/environment.py
+++ features/environment.py
@@ -75,8 +75,10 @@ def before_all(context):
         context.eps_fhir_base_url = os.path.join(select_base_url(env), EPS_FHIR_SUFFIX)
         context.pfp_base_url = os.path.join(select_base_url(env), PFP_SUFFIX)
         context.psu_base_url = os.path.join(select_base_url(env), PSU_SUFFIX)
-        if "pr-" in PULL_REQUEST_ID.lower():
-            get_url_with_pr(context, env, product)
+        if PULL_REQUEST_ID:
+            pull_request_id = PULL_REQUEST_ID.lower()
+            if "pr-" in pull_request_id:
+                get_url_with_pr(context, env, product)
     else:
         raise RuntimeError("no tests to run. Check your tags and try again")
     print("EPS: ", context.eps_fhir_base_url)
