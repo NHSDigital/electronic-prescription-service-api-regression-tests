98108b8e62bdffa1c49585a322808eae64e6d668
SeanSteberis <sean.steberis@airelogic.com>
2024-11-29 14:57:53 +0000

    added tests for the header and menu links

--- features/cpts_ui/home_page.feature
+++ features/cpts_ui/home_page.feature
@@ -8,6 +8,17 @@ Feature: I can visit the Clinical Prescription Tracker Service Website
 
   @allure.tms:https://nhsd-jira.digital.nhs.uk/browse/AEA-4515
   Scenario: user can see the footer
-    When I go to the homepage
-    Then I am on the homepage
-    And I can see the footer
+    Given I am on the homepage
+    Then I can see the footer
+
+  @allure.tms:https://nhsd-jira.digital.nhs.uk/browse/AEA-4513
+  Scenario: user can see the header
+    Given I am on the homepage
+    Then I can see the header
+    And I can see the links on the header
+
+  @allure.tms:https://nhsd-jira.digital.nhs.uk/browse/AEA-4513
+  Scenario: user sees a menu with links when the screen size is small
+    Given I am on the homepage
+    When I have a screen size of 900 pixels wide
+    Then I can see the header links in a dropdown menu
--- features/steps/cpts_ui_home_page_steps.py
+++ features/steps/cpts_ui_home_page_steps.py
@@ -1,5 +1,5 @@
 # pylint: disable=no-name-in-module
-from behave import when, then  # pyright: ignore [reportAttributeAccessIssue]
+from behave import given, when, then  # pyright: ignore [reportAttributeAccessIssue]
 from pages.home_page import HomePage
 
 
@@ -8,8 +8,15 @@ def goto_page(context):
     context.page.goto(context.cpts_ui_base_url + "site/")
 
 
-@then("I am on the homepage")
+@given("I am on the homepage")
 def verify_on_page(context):
+    goto_page(context)
+    home_page = HomePage(context.page)
+    home_page.verify_header_link()
+
+
+@then("I am on the homepage")
+def verify_on_home_page(context):
     home_page = HomePage(context.page)
     home_page.verify_header_link()
 
@@ -18,3 +25,26 @@ def verify_on_page(context):
 def i_can_see_the_footer(context):
     home_page = HomePage(context.page)
     home_page.verify_footer_is_visible()
+
+
+@then("I can see the header")
+def i_can_see_the_header(context):
+    home_page = HomePage(context.page)
+    home_page.verify_header_is_visible()
+
+
+@then("I can see the links on the header")
+def i_can_see_the_links_on_the_header(context):
+    home_page = HomePage(context.page)
+    home_page.verify_header_links_large_view()
+
+
+@when("I have a screen size of {pixel_width} pixels wide")
+def i_have_a_screen_size_of_x_pixels_wide(context, pixel_width):
+    context.page.set_viewport_size({"width": int(pixel_width), "height": 1200})
+
+
+@then("I can see the header links in a dropdown menu")
+def i_can_see_the_header_links_in_a_dropdown_menu(context):
+    home_page = HomePage(context.page)
+    home_page.verify_header_links_900_pixels_wide_view()
