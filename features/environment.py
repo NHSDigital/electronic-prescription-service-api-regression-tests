a5d5ccc7a4b8ebb66ee5c4ad4a21d19ef33664ac
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-12-12 09:01:35 +0000

    Fix: [AEA-4399] - fix tests for seperate prescribe dispense api (#192)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - only check common output in ping test as proxygen and non proxygen
    ping return different data
    - fix auth for separate prescribe and dispense
    - fix endpoints for some tests

--- features/environment.py
+++ features/environment.py
@@ -138,13 +138,14 @@ def get_url_with_pr(context, env, product):
         context.eps_fhir_base_url = os.path.join(
             INTERNAL_DEV_BASE_URL, f"{EPS_FHIR_SUFFIX}-{PULL_REQUEST_ID}"
         )
-    if product == "EPS-FHIR-PRESCRIBING":
+    if product in ["EPS-FHIR-PRESCRIBING", "EPS-FHIR-DISPENSING"]:
         context.eps_fhir_prescribing_base_url = os.path.join(
-            INTERNAL_DEV_BASE_URL, f"{EPS_FHIR_PRESCRIBING_SUFFIX}-{PULL_REQUEST_ID}"
+            INTERNAL_DEV_BASE_URL,
+            f"{EPS_FHIR_PRESCRIBING_SUFFIX}-{PULL_REQUEST_ID}",
         )
-    if product == "EPS-FHIR-DISPENSING":
         context.eps_fhir_dispensing_base_url = os.path.join(
-            INTERNAL_DEV_BASE_URL, f"{EPS_FHIR_DISPENSING_SUFFIX}-{PULL_REQUEST_ID}"
+            INTERNAL_DEV_BASE_URL,
+            f"{EPS_FHIR_DISPENSING_SUFFIX}-{PULL_REQUEST_ID}",
         )
     if product == "PFP-APIGEE":
         context.pfp_base_url = os.path.join(
