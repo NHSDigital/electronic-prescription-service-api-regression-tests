1c31fd89c1d8391c5d1927189a7960347bf1a603
Jim Wild <james.wild6@nhs.net>
2025-02-04 15:18:29 +0000

    Update naming convention

--- features/steps/cpts_ui/your_selected_role_steps.py
+++ features/steps/cpts_ui/your_selected_role_steps.py
@@ -10,10 +10,10 @@ from pages.your_selected_role import YourSelectedRole
 ############################################################################
 
 
-@given("I am on the confirm your role page")
+@given("I am on the your selected role page")
 def i_am_on_the_confirm_your_role_page(context):
     context.execute_steps("when I have a selected role")
-    context.execute_steps("then I see the 'confirm your role' page")
+    context.execute_steps("then I see the 'your selected role' page")
 
 
 ############################################################################
@@ -23,20 +23,20 @@ def i_am_on_the_confirm_your_role_page(context):
 
 @when("I click the change link next to the role text")
 def i_click_the_change_link_next_to_the_role_text(context):
-    confirm_role_page = YourSelectedRole(context.page)
-    confirm_role_page.role_change_role.click()
+    your_selected_role_page = YourSelectedRole(context.page)
+    your_selected_role_page.role_change_role.click()
 
 
 @when("I click the change link next to the org text")
 def i_click_the_change_link_next_to_the_org_text(context):
-    confirm_role_page = YourSelectedRole(context.page)
-    confirm_role_page.org_change_role.click()
+    your_selected_role_page = YourSelectedRole(context.page)
+    your_selected_role_page.org_change_role.click()
 
 
-@when("I click the confirm and continue button on the confirm role page")
-def i_click_the_confirm_and_continue_button_on_the_confirm_role_page(context):
-    confirm_role_page = YourSelectedRole(context.page)
-    confirm_role_page.confirm_button.click()
+@when("I click the confirm and continue button on the your selected role page")
+def i_click_the_confirm_and_continue_button_on_the_your_selected_role_page(context):
+    your_selected_role_page = YourSelectedRole(context.page)
+    your_selected_role_page.confirm_button.click()
 
 
 ############################################################################
@@ -44,14 +44,14 @@ def i_click_the_confirm_and_continue_button_on_the_confirm_role_page(context):
 ############################################################################
 
 
-@then("I see the 'confirm your role' page")
+@then("I see the 'your selected role' page")
 def i_see_the_confirm_your_role_page(context):
-    confirm_role_page = YourSelectedRole(context.page)
+    your_selected_role_page = YourSelectedRole(context.page)
 
-    expect(confirm_role_page.header).to_be_visible()
-    expect(confirm_role_page.role_label_cell).to_be_visible()
-    expect(confirm_role_page.org_label_cell).to_be_visible()
-    expect(confirm_role_page.org_text_cell).to_be_visible()
-    expect(confirm_role_page.role_text_cell).to_be_visible()
-    expect(confirm_role_page.role_change_role).to_be_visible()
-    expect(confirm_role_page.org_change_role).to_be_visible()
+    expect(your_selected_role_page.header).to_be_visible()
+    expect(your_selected_role_page.role_label_cell).to_be_visible()
+    expect(your_selected_role_page.org_label_cell).to_be_visible()
+    expect(your_selected_role_page.org_text_cell).to_be_visible()
+    expect(your_selected_role_page.role_text_cell).to_be_visible()
+    expect(your_selected_role_page.role_change_role).to_be_visible()
+    expect(your_selected_role_page.org_change_role).to_be_visible()
