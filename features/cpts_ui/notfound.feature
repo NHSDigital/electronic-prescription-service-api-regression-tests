ab66cc879e1edd3c679b1073366dd048bbcf21ed
Jim Wild <james.wild6@nhs.net>
2025-02-14 13:32:43 +0000

    Fix an oversight in testing

--- features/cpts_ui/notfound.feature
+++ features/cpts_ui/notfound.feature
@@ -9,9 +9,9 @@ Feature: The site has a 404 not found page
     Scenario: The user sees a "Page not found" error page when they navigate to an unknown page while logged in
         Given I am logged in as a user with multiple access roles
         When I navigate to a non-existent page
-        Then I am on the Page Not Found page
+        Then I am on the logged in Page Not Found page
 
     @deployed_only
     Scenario: The user sees a "Page not found" error page when they navigate to a url that is not under /site
         When I navigate outside the react app route
-        Then I am on the Page Not Found page
+        Then I am on the logged out Page Not Found page
--- features/steps/cpts_ui/page_not_found_steps.py
+++ features/steps/cpts_ui/page_not_found_steps.py
@@ -15,11 +15,17 @@ def i_navigate_to_a_non_app_page(context):
     context.page.goto(context.cpts_ui_base_url + "spamandeggs")
 
 
-@then("I am on the Page Not Found page")
-def i_am_on_page_not_found(context):
+@then("I am on the logged out Page Not Found page")
+def i_am_on_page_not_found_a(context):
     page = PageNotFound(context.page)
 
     expect(page.header_text).to_be_visible()
     expect(page.body1).to_be_visible()
     expect(page.body2).to_be_visible()
+
+
+@then("I am on the logged in Page Not Found page")
+def i_am_on_page_not_found_b(context):
+    context.execute_steps("then I am on the logged out Page Not Found page")
+    page = PageNotFound(context.page)
     expect(page.body3).to_be_visible()
