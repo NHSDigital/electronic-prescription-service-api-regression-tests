78a539e2f02acf83133858d520acc51e01db4377
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-02-25 08:16:57 +0000

    remove debug

--- features/steps/eps_api_steps.py
+++ features/steps/eps_api_steps.py
@@ -73,9 +73,7 @@ def i_am_an_authorised_user(context, user, app):
     if "sandbox" in context.config.userdata["env"].lower():
         return
     env = context.config.userdata["env"]
-    print("foo")
     print(user)
-    print("bar")
     if user == "api user":
         context.api_key = APIGEE_APPS[app]["client_id"]
         context.auth_method = "api_key"
