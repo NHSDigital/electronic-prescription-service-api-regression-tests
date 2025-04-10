30df7ea21f8c0015d2b4835c1efa93927ef9a46e
Jim Wild <james.wild6@nhs.net>
2025-04-10 15:53:21 +0000

    minor refactoring

--- features/steps/cpts_ui/prescription_details_steps.py
+++ features/steps/cpts_ui/prescription_details_steps.py
@@ -1,4 +1,4 @@
-from behave import given, when, then  # pyright: ignore [reportAttributeAccessIssue]
+from behave import when, then  # pyright: ignore [reportAttributeAccessIssue]
 from playwright.sync_api import expect
 
 from pages.prescription_details import PrescriptionDetailsPage
@@ -7,7 +7,7 @@ from pages.prescription_details import PrescriptionDetailsPage
 from pages.prescription_list_page import PrescriptionListPage
 
 
-@given('I go to the prescription details for prescription ID "{prescription_id}"')
+@when('I go to the prescription details for prescription ID "{prescription_id}"')
 def i_go_to_prescription_details_for_prescription_id(context, prescription_id):
     context.execute_steps(
         f"""
@@ -26,39 +26,31 @@ def i_click_to_prescription_details_page(context):
     context.page.wait_for_load_state()
 
 
-@then("The prescriber site card is {visibility}")
-def the_prescriber_site_card_is_visible(context, visibility):
+@then("The {org} site card is {visibility}")
+def site_card_visibility_check(context, org, visibility):
     page = PrescriptionDetailsPage(context.page)
 
-    if visibility == "visible":
-        expect(page.prescriber_card).to_be_visible()
-        expect(page.prescribed_from_field).to_be_visible()
-    elif visibility == "not visible":
-        expect(page.prescriber_card).not_to_be_visible()
-        expect(page.prescribed_from_field).not_to_be_visible()
-    else:
-        raise ValueError(f"Unrecognised visibility definition: {visibility}")
-
-
-@then("The dispenser site card is {visibility}")
-def the_dispenser_site_card_is_visible(context, visibility):
-    page = PrescriptionDetailsPage(context.page)
+    expect_prescribed_from_field = False
+    match org:
+        case "prescriber":
+            card = page.prescriber_card
+            expect_prescribed_from_field = True
+        case "dispenser":
+            card = page.dispenser_card
+        case "nominated dispenser":
+            card = page.nominated_dispenser_card
+        case _:
+            raise ValueError(f"Unrecognised site definition: {org}")
 
     if visibility == "visible":
-        expect(page.dispenser_card).to_be_visible()
+        expect(card).to_be_visible()
+        if expect_prescribed_from_field:
+            expect(page.prescribed_from_field)
+
     elif visibility == "not visible":
-        expect(page.dispenser_card).not_to_be_visible()
-    else:
-        raise ValueError(f"Unrecognised visibility definition: {visibility}")
+        expect(card).not_to_be_visible()
+        if expect_prescribed_from_field:
+            expect(page.prescribed_from_field)
 
-
-@then("The nominated dispenser site card is {visibility}")
-def the_nominated_dispenser_site_card_is_visible(context, visibility):
-    page = PrescriptionDetailsPage(context.page)
-
-    if visibility == "visible":
-        expect(page.nominated_dispenser_card).to_be_visible()
-    elif visibility == "not visible":
-        expect(page.nominated_dispenser_card).not_to_be_visible()
     else:
         raise ValueError(f"Unrecognised visibility definition: {visibility}")
