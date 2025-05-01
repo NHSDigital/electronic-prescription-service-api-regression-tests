7fab71be178c1157bf49e53c11c413a81a4ae66a
Kris Szlapa <kris.szlapa1@nhs.net>
2025-05-01 12:24:54 +0100

    New: [AEA-4784] - Search by NHS Number - CPTS UI (#304)
    ## Summary
    
    🎫 [AEA-4784](https://nhsd-jira.digital.nhs.uk/browse/AEA-4784) Search by
    NHS Number - CPTS UI
    **PR** https://github.com/NHSDigital/eps-prescription-tracker-ui/pull/
    
    :sparkles: New Feature
    
    ### Details
    
    🧪 Added regression tests for the implementation of the Search by NHS
    Number - CPTS UI component.
    
    ---------
    
    Co-authored-by: Sean Steberis <103416906+seansteberisal@users.noreply.github.com>

--- pages/search_for_a_prescription.py
+++ pages/search_for_a_prescription.py
@@ -24,3 +24,8 @@ class SearchForAPrescription:
         self.basic_details_search_header = page.get_by_test_id(
             "basic-details-search-heading"
         )
+        self.nhs_number_input = page.get_by_test_id("nhs-number-input")
+        self.find_patient_button = page.get_by_test_id("find-patient-button")
+        self.prescription_id_input = page.get_by_test_id("prescription-id-input")
+        self.find_prescription_button = page.get_by_test_id("find-prescription-button")
+        self.error_summary = page.get_by_test_id("error-summary")
