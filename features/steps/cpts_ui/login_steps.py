a1bf524402ec20f3bc748b945021e10aa26b2705
Kris Szlapa <kris.szlapa1@nhs.net>
2025-05-21 16:35:35 +0000

    Add the try catch TimeoutError and retry

--- features/steps/cpts_ui/login_steps.py
+++ features/steps/cpts_ui/login_steps.py
@@ -1,5 +1,6 @@
 # pylint: disable=no-name-in-module
 from behave import given, when, then  # pyright: ignore [reportAttributeAccessIssue]
+import time
 
 from features.environment import (
     MOCK_CIS2_LOGIN_ID_MULTIPLE_ACCESS_ROLES,
@@ -136,7 +137,17 @@ def the_login_is_finished(context):
         ]
         return url in valid_urls
 
-    context.page.wait_for_url(logged_in_urls, timeout=10000)
+    try:
+        context.page.wait_for_url(logged_in_urls, timeout=5000)
+    except TimeoutError:
+        time.sleep(5)
+        try:
+            context.page.wait_for_url(logged_in_urls, timeout=15000)
+        except Exception as retry_exception:
+            raise AssertionError(
+                f"Login did not complete successfully after retry: {retry_exception}"
+            ) from retry_exception
+
     context.execute_steps("then I am logged in")
 
 
