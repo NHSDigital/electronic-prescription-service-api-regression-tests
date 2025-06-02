779aae237a5c9fb80f387a331f61c6a87a9e0418
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-06-02 13:58:00 +0000

    fix lint

--- scripts/add_pr_product_to_app.py
+++ scripts/add_pr_product_to_app.py
@@ -2,10 +2,10 @@
 
 import argparse
 import os
+import urllib.parse
 import json
 import pyotp
 import requests
-import urllib.parse
 from dotenv import load_dotenv
 
 APIGEE_BASE_URL = "https://api.enterprise.apigee.com/v1/organizations/nhsd-nonprod/"
@@ -32,6 +32,8 @@ def get_headers():
 
 def get_token():
     assert secret is not None
+    assert username is not None
+    assert password is not None
     totp = pyotp.TOTP(secret)
     mfa_code = totp.now()
     headers = {
@@ -39,7 +41,11 @@ def get_token():
         "Accept": "application/json;charset=utf-8",
         "Authorization": "Basic ZWRnZWNsaTplZGdlY2xpc2VjcmV0",
     }
-    body = f"username={urllib.parse.quote(username)}&password={urllib.parse.quote(password)}&grant_type=password"
+    encoded_username = urllib.parse.quote_plus(username)
+    encoded_password = urllib.parse.quote_plus(password)
+    body = (
+        f"username={encoded_username}&password={encoded_password}&grant_type=password"
+    )
     response = requests.post(
         url=f"{SSO_LOGIN_URL}?mfa_token={mfa_code}",
         headers=headers,
