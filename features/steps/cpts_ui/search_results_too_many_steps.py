b70685ffa1103c6cbbb2f283d8b37d7103d38f4d
Kris Szlapa <kris.szlapa1@nhs.net>
2025-06-04 14:21:16 +0100

    Fix: [AEA-5360] - Create the Patient not found page - Revert (#343)
    This reverts commit 0b3105eb7dd2c3290946527a530daf3726d8283d.

--- features/steps/cpts_ui/search_results_too_many_steps.py
+++ features/steps/cpts_ui/search_results_too_many_steps.py
@@ -1,19 +1,22 @@
 # pylint: disable=no-name-in-module
-from behave import when, then  # pyright: ignore [reportAttributeAccessIssue]
+from behave import then  # pyright: ignore [reportAttributeAccessIssue]
 from playwright.sync_api import expect
-from pages.search_results_too_many import SearchResultsTooManyMessage
+from pages.search_results_too_many import SearchResultsTooManyPage
 
 
-@when("I am on the too many results page")
-@then("I am on the too many results page")
-def i_am_on_too_many_results_page(context):
-    page = SearchResultsTooManyMessage(context.page)
-    expect(page.results_page).to_be_visible()
-
-
-@then("the too many results page should display all required messages")
-def verify_too_many_results_messages(context):
-    page = SearchResultsTooManyMessage(context.page)
-    expect(page.results_message).to_be_visible()
-    expect(page.results_count_text).to_be_visible()
-    expect(page.results_alt_options).to_be_visible()
+@then("the details section shows")
+def verify_patient_details(context):
+    page = SearchResultsTooManyPage(context.page)
+    details = {
+        "First name": "First name: ",
+        "Last name": "Last name: ",
+        "DOB": "Date of birth: ",
+        "Postcode": "Postcode: ",
+    }
+    for row in context.table:
+        label = row["Heading"]
+        expected_value = row["Value"]
+        assert label in details, f"Unknown detail heading: {label}"
+        expect(page.results_details_list).to_contain_text(
+            f"{details[label]}{expected_value}"
+        )
--- pages/search_results_too_many.py
+++ pages/search_results_too_many.py
@@ -1,12 +1,10 @@
 from playwright.sync_api import Page
 
 
-class SearchResultsTooManyMessage:
+class SearchResultsTooManyPage:
     def __init__(self, page: Page):
         self.page = page
         self.page.wait_for_load_state()
 
-        self.results_page = page.get_by_test_id("too-many-results-container")
-        self.results_message = page.get_by_test_id("too-many-results-message")
-        self.results_count_text = page.get_by_test_id("too-many-results-count-text")
-        self.results_alt_options = page.get_by_test_id("too-many-results-alt-options")
+        self.results_page = page.get_by_test_id("too-many-results-page")
+        self.results_details_list = page.get_by_test_id("too-many-results-details-list")
