512faa8a1c9f7967bc7f04428e9108bfdce5a070
Kris Szlapa <kris.szlapa1@nhs.net>
2025-06-23 12:47:07 +0000

    Update PrescriptionNotFound go back link test id

--- pages/prescription_not_found.py
+++ pages/prescription_not_found.py
@@ -8,4 +8,4 @@ class PrescriptionNotFound:
 
         self.header = page.get_by_test_id("presc-not-found-header")
         self.body1 = page.get_by_test_id("presc-not-found-body1")
-        self.back_link = page.get_by_test_id("presc-not-found-backlink")
+        self.back_link = page.get_by_test_id("go-back-link")
