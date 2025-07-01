0764cf5afb7f217c105abb2881fcdf87bcef9c89
Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
2025-07-01 11:37:24 +0100

    New: [AEA-5481] - Additional tests for AEA-5481 redirect rules (#370)
    ## Summary
    
    - :sparkles: New Feature
    
    ### Details
    
    New tests to support AEA-5481 redirection rules for '/' path to '/site'.
    
    ---------
    
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
    Co-authored-by: anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>

--- features/steps/cpts_ui/page_not_found_steps.py
+++ features/steps/cpts_ui/page_not_found_steps.py
@@ -3,11 +3,18 @@ from behave import when, then  # pyright: ignore [reportAttributeAccessIssue]
 from playwright.sync_api import expect
 
 from pages.page_not_found import PageNotFound
+from pages.header import Header
+from methods.shared.common import convert_to_uri
 
 
 @when("I navigate to a non-existent page")
 def i_navigate_to_a_nonexistent_page(context):
     context.page.goto(context.cpts_ui_base_url + "site/spamandeggs")
+
+
+@when("I navigate outside the react app route with an incorrect two-segment path")
+def i_navigate_to_a_two_segment_non_app_page(context):
+    context.page.goto(context.cpts_ui_base_url + "foo/spamandeggs")
 
 
 @when("I navigate outside the react app route")
@@ -30,3 +37,39 @@ def i_am_on_page_not_found_b(context):
 
     page = PageNotFound(context.page)
     expect(page.body3).to_be_visible()
+
+
+@then("I am not redirected anywhere")
+def i_am_not_redirected_anywhere_but_see_page_not_found(context):
+    expected_url = context.cpts_ui_base_url + "foo/spamandeggs"
+
+    context.page.wait_for_url(expected_url, timeout=10000)
+
+
+@when("I navigate to the '{target}' app page outside of the site path")
+def i_navigate_to_an_app_page_outside_of_site_path(context, target):
+    uri = convert_to_uri(target)
+
+    context.page.goto(context.cpts_ui_base_url + uri)
+    header = Header(context.page)
+    header.page.is_visible(header.header)
+
+
+@then("I am redirected correctly to the site and sent to the login page")
+def i_am_redirected_to_site_and_sent_to_the_login_page(context):
+    context.page.wait_for_url(
+        f"{context.cpts_ui_base_url}site/login",
+        timeout=10000,
+    )
+
+
+@then(
+    "I am redirected correctly to the site, with URI of '{target}' correctly forwarded"
+)
+def i_am_redirected_to_site_with_uri_forwarded(context, target):
+    uri = convert_to_uri(target)
+
+    context.page.wait_for_url(
+        f"{context.cpts_ui_base_url}site/{uri}",
+        timeout=10000,
+    )
