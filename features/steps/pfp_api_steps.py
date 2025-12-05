0c8a45c68aa15baf97109894ccccdc6e9ab0b7a4
Tim Stephenson <231503406+tstephen-nhs@users.noreply.github.com>
2025-12-05 17:13:32 +0000

    feat: test for delegated access

--- features/steps/pfp_api_steps.py
+++ features/steps/pfp_api_steps.py
@@ -3,7 +3,7 @@ import json
 # pylint: disable=no-name-in-module
 from behave import when, then  # pyright: ignore [reportAttributeAccessIssue]
 
-from methods.api.pfp_api_methods import get_prescriptions
+from methods.api.pfp_api_methods import get_delegated_prescriptions, get_prescriptions
 from methods.shared.common import assert_that, get_auth
 
 
@@ -23,6 +23,16 @@ def i_request_my_prescriptions(context):
     ):
         context.nhs_number = "9449304130"
     get_prescriptions(context)
+
+
+@when("I request prescriptions of the user I have delegated access to")
+def i_request_delegated_prescriptions(context):
+    # if (
+    #     "sandbox" in context.config.userdata["env"].lower()
+    #     and "PFP" in context.config.userdata["product"].upper()
+    # ):
+    #     context.nhs_number = "9449304130"
+    get_delegated_prescriptions(context)
 
 
 @then("I can see my prescription")
@@ -46,3 +56,11 @@ def i_can_see_my_prescription(context):
     assert_that(bundle["groupIdentifier"]["value"]).is_equal_to(
         expected_prescription_id
     )
+
+
+@then("I can see the delegated prescription")
+def i_can_see_the_delegated_prescription(context):
+    # json_response = json.loads(context.response.content)
+    # with open("delegated prescription.json", "w") as f:
+    #     json.dump(json_response, f, indent=2)
+    i_can_see_my_prescription(context)
--- methods/api/pfp_api_methods.py
+++ methods/api/pfp_api_methods.py
@@ -1,4 +1,5 @@
 from methods.api.common_api_methods import get, get_headers
+from utils.random_nhs_number_generator import generate_single
 
 
 def get_prescriptions(context):
@@ -6,6 +7,18 @@ def get_prescriptions(context):
     additional_headers = {
         "x-nhs-number": context.nhs_number,
         "nhsd-nhslogin-user": "P9:" + context.nhs_number,
+    }
+
+    headers = get_headers(context, "oauth2", additional_headers)
+    context.response = get(url=url, context=context, headers=headers)
+
+
+def get_delegated_prescriptions(context):
+    url = f"{context.pfp_base_url}/Bundle"
+    additional_headers = {
+        "x-nhs-number": context.nhs_number,
+        "nhsd-nhslogin-user": "P9:" + generate_single(),  # actor != subject
+        "nhsd.subject.nhs_number": context.nhs_number,
     }
 
     headers = get_headers(context, "oauth2", additional_headers)
