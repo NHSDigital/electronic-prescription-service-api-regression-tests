ef7f145351bfa73180b923cb269dca7f5a93c8ba
tstephen-nhs <231503406+tstephen-nhs@users.noreply.github.com>
2026-02-02 11:54:57 +0000

    chore: tidy logging

--- methods/api/pfp_api_methods.py
+++ methods/api/pfp_api_methods.py
@@ -8,7 +8,6 @@ def get_prescriptions(context):
         "x-nhs-number": context.nhs_number,
         "nhsd-nhslogin-user": "P9:" + context.nhs_number,
     }
-    print(f"  with headers: {additional_headers}")
     headers = get_headers(context, "oauth2", additional_headers)
     context.response = get(url=url, context=context, headers=headers, timeout=10)
     print(f"Received response with status code: {context.response.status_code}")
